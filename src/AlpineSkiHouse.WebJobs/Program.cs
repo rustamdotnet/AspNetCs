36939b161df213d175b68e46be0316a0ce997883
rustam <rustam@centaur.kg>
2018-09-01 21:45:03 +0600

    Alpine Ski House Full

---
+++ src/AlpineSkiHouse.WebJobs/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Linq;
+
+namespace AlpineSkiHouse.WebJobs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            JobHost host = new JobHost();
+            host.RunAndBlock();
+        }
+    }
+}
---
+++ src/AlpineSkiHouse.WebJobs/WebJobs/ImageAdjustmentJob.cs
@@ -0,0 +1,23 @@
+﻿using ImageProcessor;
+using Microsoft.Azure.WebJobs;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace AlpineSkiHouse.WebJobs.WebJobs
+{
+    public class ImageAdjustments
+    {
+        public static void ResizeAndGrayscaleImage(
+            [QueueTrigger("skicard-imageprocessing")] string message,
+            [Blob("cardimages/{queueTrigger}", FileAccess.Read)] Stream imageStream,
+            [Blob("processed/{queueTrigger}", FileAccess.Write)] Stream resizedImageStream)
+        {
+            var imageFactory = new ImageFactory();
+            imageFactory.Load(imageStream)
+                .Resize(new System.Drawing.Size(100, 0))
+                .Filter(matrixFilter: ImageProcessor.Imaging.Filters.Photo.MatrixFilters.GreyScale)
+                .Save(resizedImageStream);
+        }
+    }
+}
